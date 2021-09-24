2e838feb3bba9f07296fb12aa7552986e138d340
sq <565902317@qq.com>
2021-09-24 22:41:54 +0800

    修改首页

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import styles from './index.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Button,
   Flex, FlexItem,
@@ -10,12 +10,12 @@ import {
   PreviewItem,
   SafeArea,
   Search,
-  Switch, WingBlank,
+  WingBlank,
 } from 'weui-react-v2';
 import {SearchOutlined, ScanOutlined, CloseOutlined, PlusCircleOutlined, SolutionOutlined, UploadOutlined,
   DownloadOutlined, FileTextOutlined, TeamOutlined, FileProtectOutlined, DollarOutlined, DollarCircleOutlined,
-  TransactionOutlined} from '@ant-design/icons';
-import { Badge, Calendar, Card } from 'antd';
+  TransactionOutlined, RightOutlined} from '@ant-design/icons';
+import { Affix, Badge, Calendar, Card, Col, Divider, Row, Switch } from 'antd';
 import F2 from '@antv/f2';
 // import Canvas from '@antv/f2-canvas';
 // import Avatar from 'antd/es/avatar/avatar';
@@ -69,112 +69,133 @@ const Home = () =>{
     console.log(value.format('YYYY-MM-DD'), mode);
   }
 
-  //
-  // const fontSize = 24 * (window.innerWidth / 375); // 字体适配不同屏幕
-  //
-  // const data = [{
-  //   x: '1',
-  //   y: 85
-  // }];
-  // const chart = new F2.Chart({
-  //   id: 'mountNode',
-  //   pixelRatio: window.devicePixelRatio
-  // });
-  // chart.source(data, {
-  //   y: {
-  //     max: 100,
-  //     min: 0
-  //   }
-  // });
-  // chart.axis(false);
-  // chart.tooltip(false);
-  // chart.coord('polar', {
-  //   transposed: true,
-  //   innerRadius: 0.8,
-  //   radius: 0.85
-  // });
-  // chart.guide().arc({
-  //   start: [0, 0],
-  //   end: [1, 99.98],
-  //   top: false,
-  //   style: {
-  //     lineWidth: 20,
-  //     stroke: '#ccc'
-  //   }
-  // });
-  // chart.guide().text({
-  //   position: ['50%', '50%'],
-  //   content: '85%',
-  //   style: {
-  //     fontSize: fontSize,
-  //     fill: '#1890FF'
-  //   }
-  // });
-  // chart.interval().position('x*y').size(20).animate({
-  //   appear: {
-  //     duration: 1200,
-  //     easing: 'cubicIn'
-  //   }
-  // });
-  // chart.render()
+useEffect(()=>{
+//   const data = [
+//     { genre: 'Sports', sold: 275 },
+//     { genre: 'Strategy', sold: 115 },
+//     { genre: 'Action', sold: 120 },
+//     { genre: 'Shooter', sold: 350 },
+//     { genre: 'Other', sold: 150 },
+//   ];
+//
+// // Step 1: 创建 Chart 对象
+//   const chart = new F2.Chart({
+//     id: 'myChart',
+//     pixelRatio: window.devicePixelRatio // 指定分辨率
+//   });
+//
+// // Step 2: 载入数据源
+//   chart.source(data);
+//
+// // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
+//   chart.interval().position('genre*sold').color('genre');
+//
+// // Step 4: 渲染图表
+//   chart.render();
+  const data = [{
+    name: '目标金额',
+    percent: 83.59,
+    a: '1'
+  }, {
+    name: '完成金额',
+    percent: 2.17,
+    a: '1'
+  }];
 
+  const map = {};
+  data.map(function(obj) {
+    map[obj.name] = obj.percent + '%';
+  });
+
+  const chart = new F2.Chart({
+    id: 'myChart',
+    pixelRatio: window.devicePixelRatio,
+    padding: [20, 'auto']
+  });
+  chart.source(data, {
+    percent: {
+      formatter: function formatter(val) {
+        return val + '%';
+      }
+    }
+  });
+  chart.tooltip(false);
+  chart.legend({
+    position: 'right',
+    itemFormatter: function itemFormatter(val) {
+      return val + '    ' + map[val];
+    }
+  });
+  chart.coord('polar', {
+    transposed: true,
+    innerRadius: 0.7,
+    radius: 0.85
+  });
+  chart.axis(false);
+  chart.interval().position('a*percent').color('name', ['#3BA4FF', '#afabab']).adjust('stack');
+
+  chart.guide().html({
+    position: ['50%', '45%'],
+    html: '<div style="width: 250px;height: 40px;text-align: center;">' + '<div style="font-size: 16px">完成度</div>' + '<div style="font-size: 24px">133.08 亿</div>' + '</div>'
+  });
+  chart.render();
+});
 
   return (
     <>
-      <WingBlank size="md" className={styles.list}>
+      <WingBlank size="sm" >
       <SafeArea style={{ margin: '-0.16rem', backgroundColor: '#f4f4f4', padding: '5px 0 10px' }}>
+        <Affix offsetTop={0}>
           <Search
-            // style={{width: '100%', paddingRight: '30%',float: 'left'}}
-
+            style={{paddingRight: '30%',float: 'left'}}
             placeholder="全局搜索关键字"
             onConfirm={(val) => console.log('确认输入: ', val)}
             onSearch={(val) => console.log('search: ', val)}
             onCancel={() => console.log('取消搜索')}
           >
           </Search>
+        </Affix>
       </SafeArea>
-        {/*<div className="search" style={{ width: '100%'}}>*/}
-        {/*  <Button icon={<CloseOutlined />} />*/}
-        {/*  <Button icon={<ScanOutlined />} />*/}
-        {/*  <Button icon={<SearchOutlined />} />*/}
-        {/*</div>*/}
         <Calendar onPanelChange={onPanelChange} />
-        <Preview subTitle="查看范围： 全公司" title="本月"
-                 footer={[
-                   <PreviewButton key="1">
-                     <Badge count={99} overflowCount={10}>
-                       <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}} >待执行</span>
-                     </Badge>
-                     </PreviewButton>,
-                   <PreviewButton key="2">
-                     <Badge count={99} overflowCount={10}>
-                      <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}}>进行中</span>
-                     </Badge>
-                   </PreviewButton>,
-                   <PreviewButton key="3">
-                     <Badge count={99} overflowCount={10}>
-                      <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}}>已完成</span>
-                     </Badge>
-                   </PreviewButton>,
-                   <PreviewButton key="4">
-                     <Badge count={99} overflowCount={10}>
-                      <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}}>已超时</span>
-                     </Badge>
-                   </PreviewButton>,
-                 ]}
-        >
-          <PreviewItem title="待办工作"><Switch defaultChecked={true} size="small" /></PreviewItem>
-        </Preview>
-        <Preview subTitle="查看范围： 自己" title="本月"
-        >
-          {/*<Canvas id="mountNode"></Canvas>*/}
-
-        </Preview>
+        <List >
+          <ListItem style={{padding: 3, backgroundColor: '#E6E6E6'}} extra={<div style={{fontSize: 16}} >本月</div>} ><div style={{fontSize: 16}} >查看范围： 全公司</div></ListItem>
+          <ListItem style={{padding: 5}} extra={<Switch defaultChecked={true}  />}><div style={{fontSize: 16}} >工作代办</div></ListItem>
+          <Divider style={{margin: 15}}/>
+          <div >
+            <Row>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}} >待执行</div>
+                </Badge>
+              </Col>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}}>进行中</div>
+                </Badge>
+              </Col>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}}>已完成</div>
+                </Badge>
+              </Col>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}}>已超时</div>
+                </Badge>
+              </Col>
+            </Row>
+          </div>
+        </List>
+        <List >
+          <ListItem style={{padding: 3, backgroundColor: '#E6E6E6'}} extra={<div style={{fontSize: 16}} >本月</div>} ><div style={{fontSize: 16}} >查看范围： 自己</div></ListItem>
+          <ListItem style={{padding: 10}} extra={<div style={{fontSize: 16}} >详情 {<RightOutlined />}</div>} ><div style={{fontSize: 20}} >业绩目标</div></ListItem>
+          <canvas id="myChart" width="400" height="260"></canvas>
+        </List>
         <Preview subTitle="销售简报"
         >
           <PreviewItem >
             <Flex type="flex" justify="space-between">
-              <FlexItem span={1}><div style={{textAlign: 'left'}}><PlusCircleOutlined /></div></FlexItem>
+              <FlexItem span={1}><PlusCircleOutlined /></FlexItem>
               <FlexItem span={8}><div style={{fontSize: 14, textAlign: 'left'}}>新建跟进记录</div><div style={{fontSize: 20,color: 'deepskyblue', textAlign: 'left'}} >25</div></FlexItem>
               <FlexItem span={8}><div style={{fontSize: 14, textAlign: 'left'}}>上月 100%</div><div style={{fontSize: 20,color: 'deepskyblue', textAlign: 'left'}} >0</div></FlexItem>
             </Flex>
@@ -235,38 +256,35 @@ const Home = () =>{
             </Flex>
           </PreviewItem>
         </Preview>
-        <Preview
-          footer={[
-            <PreviewButton key="1">
-              <Badge count={99} overflowCount={10}>
-                <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}} >待执行</span>
-              </Badge>
-            </PreviewButton>,
-            <PreviewButton key="2">
-              <Badge count={99} overflowCount={10}>
-                <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}}>进行中</span>
-              </Badge>
-            </PreviewButton>,
-            <PreviewButton key="3">
-              <Badge count={99} overflowCount={10}>
-                <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}}>已完成</span>
-              </Badge>
-            </PreviewButton>,
-            <PreviewButton key="4">
-              <Badge count={99} overflowCount={10}>
-                <span style={{fontSize: 50}}>15</span><span style={{fontSize: 20}}>已超时</span>
-              </Badge>
-            </PreviewButton>,
-          ]}
-        >
-          <PreviewItem title="售后简报"><Switch defaultChecked={true} size="small" /></PreviewItem>
-        </Preview>
-
-
+        <List >
+          <ListItem style={{padding: 5}} extra={<Switch defaultChecked={true}  />}><div style={{fontSize: 16}} >售后简报</div></ListItem>
+          <Divider style={{margin: 15}}/>
+          <div style={{marginTop: 5}}>
+            <Row>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}} >待执行</div>
+                </Badge>
+              </Col>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}}>进行中</div>
+                </Badge>
+              </Col>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}}>已完成</div>
+                </Badge>
+              </Col>
+              <Col span={6} style={{textAlign: 'center'}}>
+                <Badge count={99} overflowCount={10}>
+                  <div style={{fontSize: 40}}>15</div><div style={{fontSize: 15}}>已超时</div>
+                </Badge>
+              </Col>
+            </Row>
+          </div>
+        </List>
       </WingBlank>
-
-
-
   </>
   );
 };
