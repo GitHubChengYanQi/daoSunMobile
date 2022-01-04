a7d31d9ce1d320439ac3626ef57370c78c16b13a
cheng <2683941980@qq.com>
2022-01-04 13:24:36 +0800

    修改浑河api

--- .umirc.hunhe.js
+++ .umirc.hunhe.js
@@ -2,8 +2,8 @@ export default {
   define:{
     'process.env':{
       ENV:'hunhe',
-      api:'http://api.hh.gf2025.com/',
-      wxCp:'http://wx.hh.gf2025.com/cp/#/',
+      api:'https://api.hh.gf2025.com/',
+      wxCp:'https://wx.hh.gf2025.com/cp/#/',
     }
   }
 };
