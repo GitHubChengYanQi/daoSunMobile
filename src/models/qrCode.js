ed78ec1e360bfc01459147a1798b5f61c1732865
cheng <2683941980@qq.com>
2022-03-17 09:14:04 +0800

    update inventor

--- src/models/qrCode.js
+++ src/models/qrCode.js
@@ -10,6 +10,7 @@ const scan = () => new Promise((resolve, reject) => {
       needResult: 1, // 默认为0，扫描结果由企业微信处理，1则直接返回扫描结果，
       scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是条形码（一维码），默认二者都有
       success: (res) => {
+        console.log('wxScanSuccess',res);
         // 回调
         if (res.resultStr.indexOf('https') !== -1) {
           const param = res.resultStr.split('=');
@@ -22,6 +23,7 @@ const scan = () => new Promise((resolve, reject) => {
 
       },
       error: (res) => {
+        console.log('wxScanError',res);
         reject(res);
       },
     });
