d89c0f11c873f50d31714736d821c2b4e20b2b35
Genius <2683941980@qq.com>
2022-07-16 13:26:15 +0800

    修改选人样式

--- src/pages/Work/Instock/InstockAsk/Submit/components/InstockSkus/components/User/index.js
+++ src/pages/Work/Instock/InstockAsk/Submit/components/InstockSkus/components/User/index.js
@@ -4,6 +4,7 @@ import CheckUser from '../../../../../../../../components/CheckUser';
 import { RightOutline } from 'antd-mobile-icons';
 import Title from '../../../../../../../../components/Title';
 import MyCard from '../../../../../../../../components/MyCard';
+import { Avatar } from 'antd';
 
 const User = (
   {
@@ -20,10 +21,10 @@ const User = (
   return <>
     <MyCard
       titleBom={<Title className={style.title}>{title}<span>*</span></Title>}
-      extra={<div onClick={() => {
+      extra={<div className={style.alignCenter} onClick={() => {
         userRef.current.open();
       }}>
-        {id ? name : '请选择'}<RightOutline />
+        {id ? <div className={style.alignCenter}><Avatar size={20} src=''>{name.substring(0, 1)}</Avatar>{name}</div> : '请选择'}<RightOutline />
       </div>}
     />
 
