00c58cd1666de9ded349f7afa0b33c3e27ac626e
Genius <2683941980@qq.com>
2022-08-31 09:17:52 +0800

    daoxin ip

--- .umirc.daoxin.js
+++ .umirc.daoxin.js
@@ -2,7 +2,7 @@ export default {
   define:{
     'process.env':{
       ENV:'daoxin',
-      api:'http://192.168.0.230/',
+      api:'http://10.147.20.68:8881/',
       wxCp:'http://http://192.168.0.220:8001/',
       title:'道昕云',
       welcome:'欢迎使用道昕云',
