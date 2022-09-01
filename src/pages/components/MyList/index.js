df2f2055b2140b083d86ac7b2fd16ea4907d891d
Genius <2683941980@qq.com>
2022-09-01 16:41:01 +0800

    列表增加下拉刷新和返回顶部

--- src/pages/components/MyList/index.js
+++ src/pages/components/MyList/index.js
@@ -1,9 +1,10 @@
-import React, { useImperativeHandle, useState } from 'react';
+import React, { useImperativeHandle, useRef, useState } from 'react';
 import { useRequest } from '../../../util/Request';
-import { InfiniteScroll } from 'antd-mobile';
+import { FloatingBubble, InfiniteScroll, PullToRefresh } from 'antd-mobile';
 import { MyLoading } from '../MyLoading';
 import MyEmpty from '../MyEmpty';
 import style from './index.less';
+import { VerticalAlignTopOutlined } from '@ant-design/icons';
 
 let limit = 10;
 
@@ -71,13 +72,13 @@ const MyList = (
     },
   });
 
-  const submit = async (value, sorter) => {
+  const submit = async (value, sorter, pull) => {
     setHasMore(false);
     setPage(1);
     setContents([]);
     setParams(value);
     setSorter(sorter);
-    getData([], []);
+    !pull && getData([], []);
     await run({
       params: { limit, page: 1, sorter },
       data: value,
@@ -88,19 +89,43 @@ const MyList = (
     submit,
   }));
 
-  if (loading && pages === 1) {
-    return <>
-      <MyLoading skeleton title='正在加载数据...' />
-    </>;
-  }
+  // if (loading && pages === 1) {
+  //   return <>
+  //     <MyLoading skeleton title='正在加载数据...' />
+  //   </>;
+  // }
 
   if (!loading && (!data || data.length === 0) && !noEmpty) {
     return <MyEmpty height='100%' />;
   }
 
-  return <div id='list' className={style.list}>
+  const statusRecord = {
+    pulling: '用力拉',
+    canRelease: '松开吧',
+    refreshing: <MyLoading
+      imgWidth={20}
+      loaderWidth={40}
+      skeleton
+      downLoading
+      title='努力加载中...'
+      noLoadingTitle
+    />,
+    complete: '好啦',
+  };
 
-    {children}
+  const top = document.getElementById('list') ? document.getElementById('list').getBoundingClientRect().top : 0;
+
+  return <div id='list' className={style.list}>
+    <PullToRefresh
+      onRefresh={async () => {
+        await submit(params, sorter, true);
+      }}
+      renderText={status => {
+        return <div>{statusRecord[status]}</div>;
+      }}
+    >
+      {children}
+    </PullToRefresh>
 
     {error && <InfiniteScroll
       style={{ padding: noEmpty && 0 }}
@@ -127,6 +152,22 @@ const MyList = (
         }
       </>
     </InfiniteScroll>}
+
+    {top < 0 && <FloatingBubble
+      axis='xy'
+      magnetic='x'
+      style={{
+        '--initial-position-bottom': '84px',
+        '--initial-position-right': '24px',
+        '--edge-distance': '24px',
+        '--size': '40px',
+      }}
+      className={style.top}
+    >
+      <VerticalAlignTopOutlined style={{ fontSize: 24 }} onClick={() => {
+        submit(params,sorter);
+      }} />
+    </FloatingBubble>}
   </div>;
 };
 
