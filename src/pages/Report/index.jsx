dbefcc6b204b06446565cfa0ad49dcb2be465810
Genius <2683941980@qq.com>
2022-09-20 19:35:37 +0800

    统计概览页面改版

--- src/pages/Report/index.jsx
+++ src/pages/Report/index.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import MyCard from '../components/MyCard';
-import LinkButton from '../components/LinkButton';
-import ReportSwiper from './components/ReportSwiper';
-import Order from './components/Order';
 import style from './index.less';
-import TaskList from './components/TaskList';
 import { useHistory } from 'react-router-dom';
+import { Tabs } from 'antd-mobile';
+import MyCard from '../components/MyCard';
+import Stock from './components/Stock';
+import MyNavBar from '../components/MyNavBar';
 
 const Report = () => {
 
@@ -14,28 +13,28 @@ const Report = () => {
   const [title, setTitle] = useState();
 
   return <div className={style.report}>
-    <MyCard title={title} extra={<LinkButton onClick={() => {
-      history.push('/Report/StatisticalChart');
-    }}>更多</LinkButton>}>
-      <ReportSwiper titleChange={setTitle} />
-    </MyCard>
-
-    <MyCard
-      title='单据'
-      headerClassName={style.orderHeader}
-      className={style.orderCard}
-      bodyClassName={style.orderBody}
-    >
-      <Order />
-    </MyCard>
-
-    <MyCard
-      bodyClassName={style.taskBody}
-      title='任务'
-    >
-      <TaskList />
-    </MyCard>
-
+    <Tabs className={style.tabs} stretch={false}>
+      <Tabs.Tab title='库存统计' key='stock'>
+       <MyCard className={style.card} titleBom='物料统计' extra={<>历史库存：2022年07月26日 ></>}>
+         <Stock />
+       </MyCard>
+      </Tabs.Tab>
+      <Tabs.Tab title='任务统计' key='task'>
+        <div style={{ padding: 24, backgroundColor: '#fff', borderRadius: 4 }}>
+          库存统计
+        </div>
+      </Tabs.Tab>
+      <Tabs.Tab title='单据统计' key='order'>
+        <div style={{ padding: 24, backgroundColor: '#fff', borderRadius: 4 }}>
+          库存统计
+        </div>
+      </Tabs.Tab>
+      <Tabs.Tab title='异常分析' key='error'>
+        <div style={{ padding: 24, backgroundColor: '#fff', borderRadius: 4 }}>
+          库存统计
+        </div>
+      </Tabs.Tab>
+    </Tabs>
   </div>;
 };
 
