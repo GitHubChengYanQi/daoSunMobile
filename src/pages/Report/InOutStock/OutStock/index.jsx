2ea87af35ea72197d892ed3709e3c64a9b38f71b
Genius <2683941980@qq.com>
2022-10-22 09:21:34 +0800

    出库列表接口

--- src/pages/Report/InOutStock/OutStock/index.jsx
+++ src/pages/Report/InOutStock/OutStock/index.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../index.less';
 import MySearch from '../../../components/MySearch';
 import MyCard from '../../../components/MyCard';
-import { FloatingBubble, Space } from 'antd-mobile';
-import Bouncing from '../../../components/Bouncing';
-import waitInstockShop from '../../../../assets/waitInstockShop.png';
-import instockErrorShop from '../../../../assets/instockErrorShop.png';
+import { Space } from 'antd-mobile';
 import Icon from '../../../components/Icon';
 import MyFloatingBubble from '../../../components/FloatingBubble';
 import { useHistory } from 'react-router-dom';
+import MyList from '../../../components/MyList';
+
+export const OutStockDataList = { url: '/statisticalView/outstockView', method: 'POST' };
 
 const OutStock = () => {
 
   const history = useHistory();
+
+  const [list, setList] = useState([]);
 
   return <>
     <div className={style.total}>
@@ -23,44 +25,46 @@ const OutStock = () => {
 
     <MySearch placeholder='搜索' style={{ marginTop: 8, padding: '8px 12px', marginBottom: 4 }} />
 
-    {
-      [1, 2].map((item, index) => {
-        return <MyCard
-          key={index}
-          onClick={() => {
-            history.push({
-              pathname: '/Report/InOutStock/OutStock/OutStockDetail',
-              query: {
-                userId: 1,
-              },
-            });
-          }}
-          className={style.card}
-          headerClassName={style.cardHeader}
-          titleBom={<Space align='center'>
-            <div className={style.yuan} />
-            程彦祺
-          </Space>}
-        >
-          <div className={style.info}>
-            <div>
-              <div className={style.numberTitle}>领料数量</div>
-              <div className={style.cardNum}>
-                <span className='numberBlue'>65</span>类
-                <span style={{ marginLeft: 4 }} className='numberBlue'>880</span>件
+    <MyList api={OutStockDataList} data={list} getData={setList}>
+      {
+        list.map((item, index) => {
+          return <MyCard
+            key={index}
+            onClick={() => {
+              history.push({
+                pathname: '/Report/InOutStock/OutStock/OutStockDetail',
+                query: {
+                  userId: 1,
+                },
+              });
+            }}
+            className={style.card}
+            headerClassName={style.cardHeader}
+            titleBom={<Space align='center'>
+              <div className={style.yuan} />
+              程彦祺
+            </Space>}
+          >
+            <div className={style.info}>
+              <div>
+                <div className={style.numberTitle}>领料数量</div>
+                <div className={style.cardNum}>
+                  <span className='numberBlue'>65</span>类
+                  <span style={{ marginLeft: 4 }} className='numberBlue'>880</span>件
+                </div>
+              </div>
+              <div>
+                <div className={style.numberTitle}>出库数量</div>
+                <div className={style.cardNum}>
+                  <span className='numberBlue'>65</span>类
+                  <span style={{ marginLeft: 4 }} className='numberBlue'>880</span>件
+                </div>
               </div>
             </div>
-            <div>
-              <div className={style.numberTitle}>出库数量</div>
-              <div className={style.cardNum}>
-                <span className='numberBlue'>65</span>类
-                <span style={{ marginLeft: 4 }} className='numberBlue'>880</span>件
-              </div>
-            </div>
-          </div>
-        </MyCard>;
-      })
-    }
+          </MyCard>;
+        })
+      }
+    </MyList>
 
     <MyFloatingBubble><Icon type='icon-download-2-fill' /></MyFloatingBubble>;
   </>;
