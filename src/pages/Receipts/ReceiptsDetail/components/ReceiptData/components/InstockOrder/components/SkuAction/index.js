cd938ada9093fd134a2fc0b4b31d3326299f0340
Genius <2683941980@qq.com>
2022-10-22 14:58:25 +0800

    入库再次提交bug

--- src/pages/Receipts/ReceiptsDetail/components/ReceiptData/components/InstockOrder/components/SkuAction/index.js
+++ src/pages/Receipts/ReceiptsDetail/components/ReceiptData/components/InstockOrder/components/SkuAction/index.js
@@ -326,6 +326,7 @@ const SkuAction = (
               pathname: '/Work/CreateTask',
               query: {
                 createType: ERPEnums.inStock,
+                submitType: 'resubmit',
               },
               state: {
                 skus: data.map(item => {
@@ -339,6 +340,7 @@ const SkuAction = (
                     skuResult: item.skuResult,
                   };
                 }),
+                customerId: order.customerId,
                 files: isArray(order.url).map((item, index) => ({
                   mediaId: order.mediaIds[index],
                   url: item,
