dbc8f25d5442356f2921839a2470010af955327b
Genius <2683941980@qq.com>
2022-10-25 13:01:28 +0800

    每个任务的状态，标签需要用颜色区分
    #VLGO-1228

--- src/pages/Work/Stock/Task/components/TaskItem/index.js
+++ src/pages/Work/Stock/Task/components/TaskItem/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import style from './index.less';
 import { MyDate } from '../../../../../components/MyDate';
 import Icon from '../../../../../components/Icon';
-import { classNames, isArray, isObject, ToolUtil, viewWidth } from '../../../../../components/ToolUtil';
+import { classNames, isArray, ToolUtil, viewWidth } from '../../../../../components/ToolUtil';
 import MyProgress from '../../../../../components/MyProgress';
 import SkuItem from '../../../../Sku/SkuItem';
 import ShopNumber from '../../../../AddShop/components/ShopNumber';
-import receiptsOk from '../../../../../../assets/receiptsTask-ok.png';
-import receipts from '../../../../../../assets/receiptsTask.png';
-import receiptsNo from '../../../../../../assets/receiptsTask-no.png';
 import MyEllipsis from '../../../../../components/MyEllipsis';
 import MyCard from '../../../../../components/MyCard';
-import { UserName } from '../../../../../components/User';
 import { Avatar } from 'antd';
-import { Space } from 'antd-mobile';
 
 const TaskItem = (
   {
@@ -22,27 +17,20 @@ const TaskItem = (
     beginTime,
     endTime,
     coding,
-    positionSize = 0,
-    skuSize = 0,
     onClick = () => {
     },
-    skus = [],
     index,
     percent = 0,
     noSku,
     noPosition,
     noProgress,
     statusName,
-    action,
     users = [],
     userLabel,
     otherData,
-    origin = {},
-    complete,
     task = {},
   },
 ) => {
-  const originRet = isArray(origin?.parent)[0]?.ret;
   const scaleItems = Array(10).fill('');
 
   const getHour = (begin, end) => {
@@ -57,13 +45,20 @@ const TaskItem = (
   const pastTimesPercent = overtime > 0 ? 100 : ((pastTimes > 0 && total > 0) ? parseInt((pastTimes / total) * 100) : 0);
   const overScale = scaleItems.length * (pastTimesPercent / 100);
 
-  let img;
-  if (action) {
-    img = receipts;
-  } else if (complete) {
-    img = receiptsOk;
-  } else {
-    img = receiptsNo;
+  let color = 'var(--adm-color-primary)';
+
+  switch (task.status) {
+    case 49:
+      color = '#9a9a9a';
+      break;
+    case 50:
+      color = 'var(--adm-color-danger)';
+      break;
+    case 99:
+      color = '#599745';
+      break;
+    default:
+      break;
   }
 
   return <>
@@ -73,7 +68,7 @@ const TaskItem = (
       titleBom={
         <div className={style.header}>
           <div className={style.title}>{taskName}</div>
-          <div className={classNames(style.status, ['50', '49'].includes(task.status+'') && style.error)}>
+          <div style={{ border: `solid 1px ${color}`, color }} className={style.status}>
             {statusName}
           </div>
         </div>
@@ -92,7 +87,7 @@ const TaskItem = (
                 return <Avatar
                   size={18}
                   key={index}
-                  style={{marginRight:4}}
+                  style={{ marginRight: 4 }}
                   src={item.avatar || ''}
                 >
                   {item.name ? item?.name.substring(0, 1) : ''}
