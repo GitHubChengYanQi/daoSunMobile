6c2a869d1a66641753d4eb69f2af4137552a6d86
Genius <2683941980@qq.com>
2022-10-26 14:01:04 +0800

    修复问题

--- src/pages/Work/Sku/Edit/index.jsx
+++ src/pages/Work/Sku/Edit/index.jsx
@@ -18,7 +18,6 @@ import BottomButton from '../../../components/BottomButton';
 import { Message } from '../../../components/Message';
 import { spuClassificationDetail } from '../SkuDetail';
 import { SkuResultSkuJsons } from '../../../Scan/Sku/components/SkuResult_skuJsons';
-import { materialListSelect } from '../../ProcessTask/Create/components/Inventory/compoennts/AllCondition';
 
 export const editEnclosure = { url: '/sku/editEnclosure', method: 'POST' };
 export const materialListSelect = {
