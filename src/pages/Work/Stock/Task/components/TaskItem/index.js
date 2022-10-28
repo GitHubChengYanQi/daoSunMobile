626e9df282c65a7bc601d311dc26a4422b9bb0bf
Genius <2683941980@qq.com>
2022-10-28 08:40:55 +0800

    任务表单的执行人需要有文字的辨识度
    #VLGO-1260

--- src/pages/Work/Stock/Task/components/TaskItem/index.js
+++ src/pages/Work/Stock/Task/components/TaskItem/index.js
@@ -12,6 +12,7 @@ import receiptsNo from '../../../../../../assets/receiptsTask-no.png';
 import MyEllipsis from '../../../../../components/MyEllipsis';
 import MyCard from '../../../../../components/MyCard';
 import { Avatar } from 'antd';
+import { Space } from 'antd-mobile';
 
 const TaskItem = (
   {
@@ -71,7 +72,7 @@ const TaskItem = (
       titleBom={
         <div className={style.header}>
           <div className={style.title}>{taskName}</div>
-          <div className={classNames(style.status, ['50', '49'].includes(task.status+'') && style.error)}>
+          <div className={classNames(style.status, ['50', '49'].includes(task.status + '') && style.error)}>
             {statusName}
           </div>
         </div>
@@ -82,26 +83,30 @@ const TaskItem = (
     >
       <div hidden={!task.theme} className={style.theme}>{task.theme}</div>
       <div className={style.user}>
-        <div style={{ width: otherData ? '50%' : '100%' }}>
-          <MyEllipsis width='100%'>{userLabel || '执行人'}：
-            {isArray(users).length === 0 && '无'}
-            {
-              isArray(users).map((item, index) => {
-                return <Avatar
-                  size={18}
-                  key={index}
-                  style={{marginRight:4}}
-                  src={item.avatar || ''}
-                >
+        {userLabel || '执行人'}：
+        {isArray(users).length === 0 && '无'}
+        {
+          isArray(users).filter((item, index) => index < 3).map((item, index) => {
+            return <span
+              className={style.userItem}
+              key={index}
+            >
+              <Avatar
+                size={18}
+                key={index}
+                style={{ marginRight: 4 }}
+                src={item.avatar || ''}
+              >
                   {item.name ? item?.name.substring(0, 1) : ''}
-                </Avatar>;
-              })
-            }</MyEllipsis>
-        </div>
-        {otherData &&
-        <div style={{ textAlign: 'right', width: '50%' }}>
-          <MyEllipsis width='100%'>{otherData}</MyEllipsis>
-        </div>}
+                </Avatar>
+              {item.name}
+            </span>;
+          })
+        }
+        {isArray(users).length > 3 && '...'}
+      </div>
+      <div hidden={!otherData} className={style.otherData}>
+        <MyEllipsis width='100%'>{otherData}</MyEllipsis>
       </div>
       <div className={style.process} hidden={noProgress}>
         <MyProgress percent={percent} />
